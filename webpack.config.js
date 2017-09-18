60357bd0a11b4e2db7a7c2cc7693d7d4cbf1495f
Franklyn Zhu <zfranklyn@gmail.com>
2017-09-17 21:48:32 -0400

    comm

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 'use strict'
 
 module.exports = {
-	entry: __dirname + '/src/index.ts',
+	entry: __dirname + '/src/index.tsx',
 	output: {
 		filename: './dist/bundle.js',
 	},
 
+	resolve: {
+		// extensions used to resolve modules, like "require('babel')"
+		extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.webpack.js', '.web.js']
+	},
+
 	module: {
 		loaders: [
-			{ test: /\.tsx?$/, loader: 'ts-loader' },
+			{ test: /\.tsx?$/, loader: ['babel-loader', 'ts-loader'] },
 		],
 	},
+
+	devServer: {
+		contentBase: './dist'
+	}
 
 }
